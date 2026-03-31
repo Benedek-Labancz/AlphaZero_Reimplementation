48056d7764cc620889dd1e05bfa7b1a98dedd954
Benedek-Labancz <benedeklabancz@proton.me>
2026-03-31 21:40:34 +0100

    add scheduler

--- src/training/train_policy.py
+++ src/training/train_policy.py
@@ -5,7 +5,7 @@ import numpy as np
 def compute_loss(predicted_logits: torch.Tensor, v: torch.Tensor, pi_values: torch.Tensor, z: torch.Tensor):
     return mse_loss(v, z) + cross_entropy(predicted_logits, pi_values) # L2 regularization added later by optimizer
 
-def train_batch(data, policy, optimizer):
+def train_batch(data, policy, optimizer, scheduler):
     """
     Mini-batch training step,
     returning the average loss on the mini-batch.
@@ -24,4 +24,5 @@ def train_batch(data, policy, optimizer):
     )
     loss.backward()
     optimizer.step()
+    scheduler.step()
     return loss.mean()
--- test/test_train.py
+++ test/test_train.py
@@ -7,9 +7,10 @@ sys.path.insert(0, str(project_root))
 import pytest
 import numpy as np
 from torch.optim import SGD
+from torch.optim.lr_scheduler import StepLR
 
 from src.mcts.tree import Tree, Node
-from src.training.self_play import play_episode
+from src.training.self_play import self_play_episode
 from src.environments.f4ce.two_dims import TwoDims
 from src.environments.f4ce.three_dims import ThreeDims
 from src.environments.f4ce.four_dims import FourDims
@@ -22,12 +23,12 @@ def test_train_batch(EnvClass):
     root_node = Node(env=env, state=env.get_board_state())
     tree = Tree(root=root_node)
     num_squares = env.get_board_size() ** env.get_num_dimensions()
-    policy_net = PolicyScoreNetwork(num_res_blocks=4, 
+    policy_net = PolicyScoreNetwork(num_res_blocks=2, 
                                     input_size=num_squares, 
                                     output_size=num_squares, 
                                     in_channels=6)
     num_simulations = 20
-    data = play_episode(
+    data = self_play_episode(
         tree=tree,
         policy=policy_net,
         num_simulations=num_simulations,
@@ -37,10 +38,16 @@ def test_train_batch(EnvClass):
     )
 
     optimizer = SGD(params=policy_net.parameters())
+    scheduler = StepLR(
+        optimizer=optimizer,
+        step_size=2,
+        gamma=1e-3
+    )
     mean_loss = train_batch(
         data=data,
         policy=policy_net,
-        optimizer=optimizer
+        optimizer=optimizer,
+        scheduler=scheduler
     )
     assert mean_loss > 0
     for param in policy_net.parameters():
